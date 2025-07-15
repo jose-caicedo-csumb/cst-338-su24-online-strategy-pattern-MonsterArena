a5859837a1b3fcf180f7a1ded2dda4214a093640
jcaiced <jcaiced@gmail.com>
2025-07-14 17:42:25 -0700

    added code to Ability and Attack

--- src/main/java/Abilities/Ability.java
+++ src/main/java/Abilities/Ability.java
@@ -1,8 +1,16 @@
+/**
+ * Author: Jose Caicedo
+ * created: 07/13/25
+ * Explanation: This is a tagging interface used to mark all ability-related
+ * interfaces or classes. It does not declare any methods, but can be used to group related types
+ * under a common interface.
+ */
 package Abilities;
 
 /**
- * Author: Jose Caicedo Created on: 7/14/25 Description:
+ * A tagging interface for all abilities.
+ * This allows future flexibility — for example, checking if an object is an Ability.
  */
-public class Ability {
+public interface Ability {
 
 }
--- src/main/java/Abilities/Attack.java
+++ src/main/java/Abilities/Attack.java
@@ -1,8 +1,25 @@
+/**
+ * Author: Jose Caicedo
+ * Created on: 7/13/25
+ * Explanation: This interface represents any kind of
+ * attack a monster can perform. It extends the Ability interface, making it part of the monster's
+ * abilities system.
+ */
+
 package Abilities;
 
-/**
- * Author: Jose Caicedo Created on: 7/14/25 Description:
- */
-public class Attack {
+import monsters.Monster;
 
+/**
+ * Attack defines a method that any implementing class must use to attack a target Monster.
+ */
+public interface Attack extends Ability {
+
+  /**
+   * Performs an attack on the target monster.
+   *
+   * @param target the Monster that is being attacked
+   * @return the damage dealt as an Integer
+   */
+  Integer attack(Monster target);
 }
