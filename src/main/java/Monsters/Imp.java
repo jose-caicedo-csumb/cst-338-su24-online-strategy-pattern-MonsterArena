995c535fb9cc9187b638120fc71ebdc006f9e039
jcaiced <jcaiced@gmail.com>
2025-07-14 17:57:49 -0700

    added Imp and Kobold

--- src/main/java/Monsters/Imp.java
+++ src/main/java/Monsters/Imp.java
@@ -1,8 +1,51 @@
-package Monsters;
+/**
+ * Author: Jose Caicedo
+ * Created on: 7/13/25
+ * Explanation: This class represents a specific type of monster:
+ * Imp. It initializes attributes using randomized values and sets its attack to
+ * MeleeAttack.
+ */
+
+package monsters;
+
+import Abilities.MeleeAttack;
+import java.util.HashMap;
 
 /**
- * Author: Jose Caicedo Created on: 7/14/25 Description:
+ * Imp is a Monster that uses melee attacks.
+ * Its strength, defense, and agility are initialized using randomized values within specified ranges.
  */
-public class Imp {
+public class Imp extends Monster {
 
+  /**
+   * Constructor for the Imp monster.
+   * Sets up randomized stats and assigns a MeleeAttack ability.
+   *
+   * @param maxHP maximum hit points for the Imp
+   * @param xp experience points
+   * @param items inventory items
+   */
+  public Imp(Integer maxHP, Integer xp, HashMap<String, Integer> items) {
+    super(maxHP, xp, items);
+
+    Integer maxStr = 15;
+    Integer maxDef = 6;
+    Integer maxAgi = 3;
+
+    attack = new MeleeAttack(this);
+
+    strength = super.getAttribute(strength, maxStr);
+    defense = super.getAttribute(defense, maxDef);
+    agility = super.getAttribute(agility, maxAgi);
+  }
+
+  /**
+   * Provides a string representation of the Imp, including its current HP.
+   *
+   * @return formatted string
+   */
+  @Override
+  public String toString() {
+    return "Monsters.Imp has : " + super.toString();
+  }
 }
--- src/main/java/Monsters/Kobold.java
+++ src/main/java/Monsters/Kobold.java
@@ -1,8 +1,49 @@
-package Monsters;
+/**
+ * Author: Jose Caicedo
+ * Created on: 7/13/25
+ * Explanation: This class represents a Kobold monster. It
+ * initializes stats using randomized values and uses a RangedAttack ability.
+ */
+
+package monsters;
+
+import Abilities.RangedAttack;
+import java.util.HashMap;
 
 /**
- * Author: Jose Caicedo Created on: 7/14/25 Description:
+ * Kobold is a Monster that specializes in ranged attacks.
+ * Its strength, defense, and agility are set using randomized values based on custom max values.
  */
-public class Kobold {
+public class Kobold extends Monster {
 
+  /**
+   * Constructor for the Kobold monster.
+   * Sets up randomized stats and assigns a RangedAttack ability.
+   *
+   * @param maxHP maximum hit points for the Kobold
+   * @param xp experience points
+   * @param items inventory items
+   */
+  public Kobold(Integer maxHP, Integer xp, HashMap<String, Integer> items) {
+    super(maxHP, xp, items);
+
+    Integer maxStr = 8;
+    Integer maxDef = 5;
+    Integer maxAgi = 12;
+
+    attack = new RangedAttack(this);
+    strength = super.getAttribute(strength, maxStr);
+    defense = super.getAttribute(defense, maxDef);
+    agility = super.getAttribute(agility, maxAgi);
+  }
+
+  /**
+   * Provides a string representation of the Kobold, including its current HP.
+   *
+   * @return formatted string
+   */
+  @Override
+  public String toString() {
+    return "Monsters.Kobold has : " + super.toString();
+  }
 }
