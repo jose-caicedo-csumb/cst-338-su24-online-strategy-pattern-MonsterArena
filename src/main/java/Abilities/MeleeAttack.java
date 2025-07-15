e7d101ca9bcb0140b49c919597285e03cf925b73
jcaiced <jcaiced@gmail.com>
2025-07-14 17:54:22 -0700

    added MeleeAttack and RangedAttack

--- src/main/java/Abilities/MeleeAttack.java
+++ src/main/java/Abilities/MeleeAttack.java
@@ -1,8 +1,42 @@
+/**
+ * Author: Jose Caicedo
+ * Created on: 7/13/25
+ * Explanation: This class represents a melee attack
+ * ability. It implements the Attack interface and performs a basic strength vs. defense
+ * calculation.
+ */
+
 package Abilities;
 
-/**
- * Author: Jose Caicedo Created on: 7/14/25 Description:
- */
-public class MeleeAttack {
+import monsters.Monster;
 
+/**
+ * MeleeAttack is a concrete implementation of the Attack interface.
+ * It uses the attacker's strength and the target's defense to determine damage.
+ */
+public class MeleeAttack implements Attack {
+
+  Monster attacker;
+
+  /**
+   * Constructor assigns the attacking monster.
+   *
+   * @param attacker the monster performing the attack
+   */
+  public MeleeAttack(Monster attacker) {
+    this.attacker = attacker;
+  }
+
+  /**
+   * Executes the melee attack and prints the action.
+   *
+   * @param target the monster being attacked
+   * @return the damage dealt (strength - defense)
+   */
+  @Override
+  public Integer attack(Monster target) {
+    String message = attacker + " uses a melee attack on " + target;
+    System.out.println(message);
+    return attacker.getStrength() - target.getDefense();
+  }
 }
--- src/main/java/Abilities/RangedAttack.java
+++ src/main/java/Abilities/RangedAttack.java
@@ -1,8 +1,40 @@
+/**
+ * Author: Jose Caicedo
+ * Created on: 7/13/25
+ * Explanation: This class represents a ranged attack
+ * ability. It implements the Attack interface and calculates damage using agility.
+ */
+
 package Abilities;
 
-/**
- * Author: Jose Caicedo Created on: 7/14/25 Description:
- */
-public class RangedAttack {
+import monsters.Monster;
 
+/**
+ * RangedAttack is a type of Attack that uses agility to determine damage.
+ */
+public class RangedAttack implements Attack {
+
+  Monster attacker;
+
+  /**
+   * Constructor that sets the attacking monster.
+   *
+   * @param attacker the monster performing the ranged attack
+   */
+  public RangedAttack(Monster attacker) {
+    this.attacker = attacker;
+  }
+
+  /**
+   * Executes the ranged attack and prints out the action.
+   *
+   * @param target the monster being attacked
+   * @return the damage dealt (attacker's agility - target's agility)
+   */
+  @Override
+  public Integer attack(Monster target) {
+    String message = attacker + " uses a ranged attack on " + target;
+    System.out.println(message);
+    return attacker.getAgility() - target.getAgility();
+  }
 }
